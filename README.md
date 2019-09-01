a659726b219241e5cba6c8fdb887dc87282b1f38
fsw <frank@wawrzak.com>
2019-09-01 19:24:09 +0200

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # swift_ids
+
+## abstract
+
 Convert int to human friendly id string and back.
+
+| integer | swift_id |
+|---|--------|
+| 0 | GX08 |
+| 1 | HY9U |
+| 2 | JA2Q |
+| 5 | D9YU |
+| 1000 | SF7B |
+| 10000000 | GM1Y3Q |
+
+The generated ID is:
+
+* fast to convert back and forth (algorithm uses mostly binary operations)
+* seems random (end user can't browse trough entities easily)
+* user can't tell how many entities there are
+* shorter than original int (5 characters can save 8388608 ids)
+* easy to write down / spell over the phone because generated string uses:
+.* only uppercase letters,
+.* no confusing digits: O=0 and I=L=1
+.* no two consecutive characters are the same
+* avoid accidental profanities (and words in general)
+* minimum length is configurable
+
+## usage
+```dart
+var encode = new Id(5)
+print("${encode.value} == ${encode.toString()}"); //5 == D9YU
+var decode = new Id.fromString('D9YU');
+print("${decode.value} == ${decode.toString()}"); //4 == C84X
+
+//to configurable minimum lenght:
+var encode = new Id(5, minLength:10, zero:0x55555555555)
+print("${encode.value} == ${encode.toString()}"); //5 == HBQ8H7E85B
+//same zero/minLength need to be used when decoding
+```
+
+`zero` is an initial value that is XORed with input that can be used to:
+* make separate entities ids not overlap (so user with id=5 won't have same hash as message with id=5)
+* mangle initial integer to prevent repetitions for small integers with large `minLength`
+* `zero` should have maximum `(5 * minLength) - (2 * (minLength / 3))` bits.
---
+++ lib/swift_ids.dart
@@ -0,0 +1,108 @@
+library swift_ids;
+
+final String _encodeChars = "0123456789QVXYABCDEFGHJKMNPRSTWZU";
+
+final Map<String, int> _decodeMap = const {
+  '0': 0,  '1': 1,  '2': 2,  '3': 3,  '4': 4,  '5': 5,  '6': 6, '7': 7,
+  '8': 8,  '9': 9,  'Q': 10, 'V': 11, 'X': 12, 'Y': 13,
+  'A': 14,  'B': 15,  'C': 16,  'D': 17,  'E': 18,  'F': 19,
+  'G': 20,  'H': 21,  'J': 22,  'K': 23,  'M': 24,  'N': 25,
+  'P': 26,  'R': 27,  'S': 28,  'T': 29,  'W': 30,  'Z': 31,
+  'U': 32
+};
+
+class Id {
+  int value;
+  int minLength;
+  int zero;
+
+  Id(this.value, {this.minLength = 4, this.zero = 0x11111});
+
+  Id.fromString(String string, {this.minLength = 4, this.zero = 0x11111}) {
+    value = _decode(string);
+  }
+
+  static String sanitizeHumanString(String string) {
+    string = string.toUpperCase();
+    string.replaceAll('O', '0');
+    string.replaceAll('I', '1');
+    string.replaceAll('L', '1');
+    return string;
+  }
+
+  int _decode(String string) {
+    int value = 0;
+    int chars = 0;
+    int prevDecoded = 0;
+    int offset = 0;
+    int mangler = 0x5;
+    string.split('').forEach((String char){
+      if (!_decodeMap.containsKey(char)) {
+        throw new Exception('unknow character: ' + char);
+      }
+      int decoded = _decodeMap[char];
+      if (decoded == 32) {
+        decoded = prevDecoded;
+      }
+      if (chars ++ == 2) {
+        if (decoded & 16 > 0) {
+          throw new Exception('checksum error');
+        }
+        if ((decoded & 0x7 < 6) && (decoded >> 3) != (prevDecoded & 1)) {
+          throw new Exception('checksum error');
+        }
+        value = value + (((decoded & 0x7) ^ mangler) << offset);
+        mangler = decoded & 0x7;
+        offset += 3;
+        chars = 0;
+      } else {
+        value = value + ((decoded ^ mangler) << offset);
+        mangler = decoded & 0x7;
+        offset += 5;
+      }
+      prevDecoded = _decodeMap[char];
+    });
+    return value ^ zero;
+  }
+
+  String _encode(int value) {
+
+    value = value ^ zero;
+
+    String ret = "";
+    int mangler = 0x5;
+    int prevCharCode = 0;
+    int charCode;
+    int chars = 0;
+    while (value > 0 || ret.length < minLength) {
+
+      if (chars ++ == 2) {
+        charCode = (value & 7) ^ mangler;
+        value >>= 3;
+        if (charCode < 6) {
+          //checksum
+          charCode += (prevCharCode & 1) << 3;
+        }
+        chars = 0;
+      } else {
+        charCode = (value & 31) ^ mangler;
+        value >>= 5;
+      }
+      mangler = charCode & 0x7;
+      if (charCode == prevCharCode) {
+        charCode = 32;
+      }
+      prevCharCode = charCode;
+      ret += _encodeChars[charCode];
+    }
+    return ret;
+  }
+
+  int toInt() {
+     return value;
+  }
+
+  String toString() {
+     return _encode(value);
+  }
+}
